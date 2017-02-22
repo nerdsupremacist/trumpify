521197c70b3596728fb6ba0f27119db114191d62
Mathias Quintero <mathias.quintero@tum.de>
2017-02-22 04:07:17 +0100

    Adding first draft of web page

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var tagger = require('./util/tag.js');
 var nicknamer = require('./util/nicknamer.js');
 var words = require('./util/synonyms.js');
 
-function process(text, callback) {
+
+
+function trumpify(text, callback) {
     tagger(text, function(text, sentiment) {
         nicknamer(text, sentiment, function(text, keywords) {
             words(text, keywords, callback);
         });
     });
 }
-module.exports = process;
+
+window.trumpify = function(text, callback) {
+    trumpify(text, callback);
+};
+
+module.exports = trumpify;
